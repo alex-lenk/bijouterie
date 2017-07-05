a5c007a43b148a17461cd5e56eb9176c6293f2e9
artlenk.ru@gmail.com <df5ukoi7ht>
2017-07-05 15:18:39 +0300

    Initial commit

---
+++ src/js/common.js
@@ -0,0 +1,46 @@
+/*
+$(document).ready(function () {
+    $('.single-item').slick({
+        dots: true,
+        arrows: false,
+        fade: true
+    });
+
+    $('.carousel-gallery').slick({
+        dots: true,
+        arrows: false,
+        fade: true
+    });
+
+    $(".navbar-toggle").click(
+        function () {
+            $('.menu').toggleClass("menu-active");
+        }
+    );
+});
+
+var navPos, winPos, navHeight, nav = $(".nav-menu");
+
+function refreshVar() {
+    navPos = nav.offset().top;
+    navHeight = nav.outerHeight(true);
+}
+
+refreshVar();
+$(window).resize(refreshVar);
+
+$('<div class="clone-nav"></div>').insertBefore(".nav-left").css("height", navHeight).hide();
+
+$(window).scroll(function () {
+    winPos = $(window).scrollTop();
+
+    if (winPos >= navPos) {
+        nav.addClass("fixed shadow");
+        $(".clone-nav").show();
+    } else {
+        nav.removeClass("fixed shadow");
+        $(".clone-nav").hide();
+    }
+});
+
+*/
---
+++ src/js/lib.js
@@ -0,0 +1,9 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+// = ../../bower_components/slick-carousel/slick/slick.js
+
+// = lib/bootstrap.min.js
+
+// = lib/smoothscroll.js
+// = lib/jquery.fancybox.min.js
+// = lib/svgxuse.min.js
